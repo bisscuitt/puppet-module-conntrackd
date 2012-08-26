3bf6a309f541055a82c7e834e0cd1b617f2d2633
Ian Bissett <bisscuitt@gmail.com>
2012-08-27 09:56:47 +1000

    confining nf_conntrack_max fact to the linux kernel only

--- lib/facter/nf_conntrack_max.rb
+++ lib/facter/nf_conntrack_max.rb
@@ -1,5 +1,6 @@
 # get the value of /proc/sys/net/netfilter/nf_conntrack_max
 Facter.add("nf_conntrack_max") do
+  confine :kernel => :linux
   setcode do
     Facter::Util::Resolution.exec('/bin/cat /proc/sys/net/netfilter/nf_conntrack_max').chomp
   end
