a3894dae63bbd752adb30b0ef8caafa87a72089b
mh <mh@immerda.ch>
2014-12-17 20:17:23 +0100

    make this useable on more systems

--- lib/facter/nf_conntrack_max.rb
+++ lib/facter/nf_conntrack_max.rb
@@ -1,7 +1,11 @@
 # get the value of /proc/sys/net/netfilter/nf_conntrack_max
-Facter.add("nf_conntrack_max") do
-  confine :kernel => :linux
-  setcode do
-    Facter::Util::Resolution.exec('/bin/cat /proc/sys/net/netfilter/nf_conntrack_max').chomp
+f = [ '/proc/sys/net/netfilter/nf_conntrack_max',
+      '/proc/sys/net/ipv4/ip_conntrack_max' ].find{|f| File.exists?(f) }
+if f
+  Facter.add("nf_conntrack_max") do
+    confine :kernel => :linux
+    setcode do
+      File.read(f).chomp
+    end
   end
 end
